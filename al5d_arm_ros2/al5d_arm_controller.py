a3649c74190582081ccea2c872585e855df8240d
kimbring2 <kimbring2@gmail.com>
2023-04-18 05:30:17 +0900

    uart test

--- al5d_arm_ros2/al5d_arm_controller.py
+++ al5d_arm_ros2/al5d_arm_controller.py
@@ -9,35 +9,43 @@ class AL5DSerialController(Node):
     def __init__(self):
         super().__init__('al5d_arm_controller')
 
-        
         self.declare_parameter('serial_port', '/dev/ttyUSB0')
         self.declare_parameter('baud_rate', 115200)
 
         serial_port = self.get_parameter('serial_port').value
         baud_rate = self.get_parameter('baud_rate').value
 
-        self.serial = serial.Serial(serial_port, baud_rate)
+        #self.serial = serial.Serial(serial_port, baud_rate)
+        self.serial = serial.Serial(
+            port = '/dev/ttyUSB0',
+            baudrate = 115200,
+            timeout = 10,
+            xonxoff = False
+        )
+
         time.sleep(2)  # Wait for the connection to be established
         
         # ros2 topic pub -r 10 /command std_msgs/msg/String "{data: '^b090s135e010w090g120w000$'}" --once
         self.subscription = self.create_subscription(
             String,
-            'command',
+            'al5d_command',
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
+        self.get_logger().info('AL5D command: "%s"' % msg.data)
         print("type(msg.data): ", type(msg.data))
 
-        #command = '^b060s135e010w090g120w000$'
+        #command = '^b045s135e010w090g120w000$'
+        #command = 'test'
         command = msg.data
         command += '\n'
-        self.serial.write((command).encode('utf-8'))
+        #self.serial.write((command).encode('utf-8'))
+        self.serial.write((command).encode(encoding='ascii', errors='strict'))
 
         #self.serial.write((msg.data).encode('utf-8'))
-        time.sleep(5.0)
+        #time.sleep(5.0)
 
     def send_command(self, command):
         self.serial.write((command).encode('utf-8'))
